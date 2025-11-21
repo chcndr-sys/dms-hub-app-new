151d315b700e3c6f510aa31760c922579c80c66d
Chcndr <94785821+Chcndr@users.noreply.github.com>
2025-11-20 20:17:01 -0500

    Force CDN cache invalidation - add timestamp comment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,7 @@ import superjson from "superjson";
 import App from "./App";
 import { getLoginUrl } from "./const";
 import "./index.css";
+// Force CDN cache invalidation - timestamp: 1763685966
 
 const queryClient = new QueryClient();
 
